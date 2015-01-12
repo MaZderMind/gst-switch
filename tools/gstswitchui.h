f579c74f96c66c2a06c7fdcd0a322fed88b78949
MaZderMind <github@mazdermind.de>
2015-01-12 23:45:09 +0100

    Strip the complete UI and replace it with a minimal Glade-Example to work on

--- tools/gstswitchui.h
+++ tools/gstswitchui.h
@@ -30,72 +30,12 @@
 
 #include <gdk/gdkx.h>
 #include <gtk/gtk.h>
-#include "gstswitchclient.h"
-#include "gstworker.h"
-#include "gstvideodisp.h"
-#include "gstaudiovisual.h"
 
-// Only allowed to use enum and to_string methods from this file.
-#include "gstcomposite.h"
-
-#define GST_TYPE_SWITCH_UI (gst_switch_ui_get_type ())
-#define GST_SWITCH_UI(object) (G_TYPE_CHECK_INSTANCE_CAST ((object), GST_TYPE_SWITCH_UI, GstSwitchUI))
-#define GST_SWITCH_UI_CLASS(class) (G_TYPE_CHECK_CLASS_CAST ((class), GST_TYPE_SWITCH_UI, GstSwitchUIClass))
-#define GST_IS_SWITCH_UI(object) (G_TYPE_CHECK_INSTANCE_TYPE ((object), GST_TYPE_SWITCH_UI))
-#define GST_IS_SWITCH_UI_CLASS(class) (G_TYPE_CHECK_CLASS_TYPE ((class), GST_TYPE_SWITCH_UI))
-
-typedef struct _GstSwitchUI GstSwitchUI;
-typedef struct _GstSwitchUIClass GstSwitchUIClass;
-
-/**
- *  @class GstSwitchUI
- *  @struct _GstSwitchUI
- *  @brief The gst-switch-ui program.
- */
-struct _GstSwitchUI
+typedef struct _UiData UiData;
+struct _UiData
 {
-  GstSwitchClient base;
-
-  GtkCssProvider *css;
-
-  GtkWidget *window;
-  GtkWidget *compose_view;
-  GtkWidget *compose_overlay;
-  GtkWidget *preview_box;
-  GtkWidget *status;
-
-  GMutex select_lock;
-  GtkWidget *selected;
-
-  GMutex audio_lock;
-  gint audio_port;
-  GstAudioVisual *audio;
-  GstClockTime audio_endtime;
-  gdouble audio_value;
-  gint audio_stuck_count;
-
-  GMutex compose_lock;
-  GstVideoDisp *compose;
-
-  guint32 tabtime;
-  GstCompositeMode compose_mode;
-  gint timer;
-
-  GMutex faces_lock, tracking_lock;
-  GVariant *faces;
-  GVariant *tracking;
+  GtkWidget *mainwindow;
+  GtkWidget *quitdialog;
 };
-
-/**
- *  @class GstSwitchUIClass
- *  @struct _GstSwitchUIClass
- *  @brief The class of GstSwitchUI.
- */
-struct _GstSwitchUIClass
-{
-  GstSwitchClientClass base_class; /*!< The base class. */
-};
-
-GType gst_switch_ui_get_type (void);
 
 #endif //__GST_SWITCH_UI_H__
